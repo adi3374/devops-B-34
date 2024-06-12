f3b887bf4e76628201fc5e4b1dd83b3b32bde922
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-06-12 13:45:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.53.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
   profile = "devops-tf"
 }
-
+# create iam user and groups
 resource "aws_iam_user" "demo" {
-  name = "mangesh"
+  name = "mahesh"
 
 }
 
 resource "aws_iam_user" "demo1" {
-  name = "omkar"
+  name = "salonie"
 }
 
 resource "aws_iam_user" "demo2" {
-  name = "aniket"
+  name = "ankita"
 }
 
 resource "aws_iam_group" "grp" {
-  name = "devops-b34"
+  name = "team-dev"
 }
 
 resource "aws_iam_user_group_membership" "grpadd" {
    user = aws_iam_user.demo1.name
-
+   
    groups = [
           aws_iam_group.grp.name
          ]
@@ -40,3 +49,32 @@ resource "aws_iam_user_group_membership" "grpadd3" {
          aws_iam_group.grp.name
        ]
 }
+
+
+# code for creating s3 bucket
+resource "aws_s3_bucket" "bucket-1" {
+  bucket = "mangesh-baltiwala"
+   
+}
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket =aws_s3_bucket.bucket-1.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_acl" "achal" {
+  bucket = aws_s3_bucket.bucket-1.id
+  acl = "private"
+}
+
+
+
+
+
+
+
+
+
+
+
+
